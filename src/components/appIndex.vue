313b633a2f5a23cd4967a952a45fe30af0d3ba0f
farid rohim <122333775+Faridrohim@users.noreply.github.com>
2024-04-17 10:14:02 +0700

    Update appIndex.vue

--- src/components/appIndex.vue
+++ src/components/appIndex.vue
@@ -327,7 +327,7 @@ z-index: 1;
 }
 nav ul {
   font-size: 14px;
-  top: 15px;
+  top: 12px;
 }
 .logo {
  height: 20px;
@@ -392,7 +392,7 @@ display: flex;
 justify-content: start;
 font-family: 'Bebas Neue';
 font-size: 30px;
-top: 15px;
+top: 12px;
 }
 
 nav ul li  {
@@ -400,7 +400,7 @@ color: #FFFFFF;
 position: relative;
 display: inline-block;
 margin-right: 40px;
-top: 15px;
+top: 12px;
 }
 
 nav ul li:hover {
